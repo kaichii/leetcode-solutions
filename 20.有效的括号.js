354e19f9461c0bfbfb7aec959e76aa2e86212374
kaichi <hellokitty321ds@gmail.com>
2022-04-28 10:38:51 +0800

    20

---
+++ 20.有效的括号.js
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (44.53%)
+ * Likes:    3210
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "()"
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "()[]{}"
+ * 输出：true
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：s = "(]"
+ * 输出：false
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：s = "([)]"
+ * 输出：false
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：s = "{[]}"
+ * 输出：true
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * s 仅由括号 '()[]{}' 组成
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
+      stack.push(s[i]);
+    } else if (!matches(stack.pop(), s[i])) {
+      return false;
+    }
+  }
+
+  return !stack.length;
+};
+
+function matches(a, b) {
+  return (
+    (a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}')
+  );
+}
+// @lc code=end
