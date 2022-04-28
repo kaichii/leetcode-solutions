c63d4527373ac68e13cdb21c8e7f962835d64b84
kaichi <hellokitty321ds@gmail.com>
2022-04-28 19:29:26 +0800

    stash 28

---
+++ 28.实现-str-str.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 实现 strStr()
+ *
+ * https://leetcode-cn.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (40.13%)
+ * Likes:    1403
+ * Dislikes: 0
+ * Total Accepted:    630.5K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * 实现 strStr() 函数。
+ *
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0
+ * 开始）。如果不存在，则返回  -1 。
+ *
+ *
+ *
+ * 说明：
+ *
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ *
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf()
+ * 定义相符。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：haystack = "hello", needle = "ll"
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：haystack = "aaaaa", needle = "bba"
+ * 输出：-1
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：haystack = "", needle = ""
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= haystack.length, needle.length <= 10^4
+ * haystack 和 needle 仅由小写英文字符组成
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (!needle) return 0;
+
+  let ans;
+
+  let i = 0;
+
+  while (i < haystack.length) {
+    for (let j = 0; j < needle.length; j++) {
+      console.log(i, j, haystack[i + j], needle[j]);
+      if (haystack[i + j] === needle[j]) {
+        if (j === needle.length - 1) {
+          ans = i;
+        }
+      } else {
+        i = i + j;
+        break;
+      }
+    }
+    i++;
+  }
+
+  return ans ?? -1;
+};
+// @lc code=end
