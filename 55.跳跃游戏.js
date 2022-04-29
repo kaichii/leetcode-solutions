c7cebcb1a2dee89dbfcf17230336970a33ef46f7
kaichi <hellokitty321ds@gmail.com>
2022-04-29 10:36:24 +0800

    55

---
+++ 55.跳跃游戏.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ *
+ * https://leetcode-cn.com/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (43.47%)
+ * Likes:    1808
+ * Dislikes: 0
+ * Total Accepted:    479.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let max = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    // 在范围
+    if (i <= max) {
+      max = Math.max(max, i + nums[i]);
+    } else {
+      break;
+    }
+  }
+
+  return max >= nums.length - 1;
+};
+// @lc code=end
