180432c65ee9204ab88230d29afa8029107bc7b9
kaichi <hellokitty321ds@gmail.com>
2022-09-05 17:32:50 +0800

    102

---
+++ solutions/102.二叉树的层序遍历.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (65.08%)
+ * Likes:    1448
+ * Dislikes: 0
+ * Total Accepted:    664.1K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (root == null) return [];
+
+  const ans = [],
+    q = [root];
+
+  while (q.length) {
+    const len = q.length;
+    ans.push(q.map((m) => m.val));
+
+    for (let i = 0; i < len; i++) {
+      const curr = q.shift();
+
+      if (curr.left !== null) {
+        q.push(curr.left);
+      }
+      if (curr.right !== null) {
+        q.push(curr.right);
+      }
+    }
+  }
+
+  return ans;
+};
+// @lc code=end
