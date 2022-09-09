00bc4783ba6ba41792b585c0f8a776ba4431b384
kaichi <hellokitty321ds@gmail.com>
2022-09-09 11:14:54 +0800

    113

---
+++ solutions/113.路径总和-ii.js
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=javascript
+ *
+ * [113] 路径总和 II
+ *
+ * https://leetcode.cn/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (63.24%)
+ * Likes:    839
+ * Dislikes: 0
+ * Total Accepted:    292.8K
+ * Total Submissions: 463K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：[[5,4,11,2],[5,8,4,5]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,2], targetSum = 0
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点总数在范围 [0, 5000] 内
+ * -1000
+ * -1000
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function (root, targetSum) {
+  let sum = 0;
+  const ans = [],
+    level = [];
+
+  const traverse = (r) => {
+    if (r == null) return;
+
+    sum += r.val;
+    level.push(r.val);
+    traverse(r.left);
+    traverse(r.right);
+    if (r.left == null && r.right == null && sum === targetSum) {
+      ans.push([...level]);
+    }
+    sum -= r.val;
+    level.pop();
+  };
+
+  traverse(root);
+
+  return ans;
+};
+// @lc code=end
