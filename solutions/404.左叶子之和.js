70348da0c5798237388c5ca809b01937c575d7dc
kaichi <hellokitty321ds@gmail.com>
2022-09-09 11:38:54 +0800

    404

---
+++ solutions/404.左叶子之和.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=javascript
+ *
+ * [404] 左叶子之和
+ *
+ * https://leetcode.cn/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (61.42%)
+ * Likes:    495
+ * Dislikes: 0
+ * Total Accepted:    179.9K
+ * Total Submissions: 292.8K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定二叉树的根节点 root ，返回所有左叶子之和。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入: root = [3,9,20,null,null,15,7]
+ * 输出: 24
+ * 解释: 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: root = [1]
+ * 输出: 0
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 节点数在 [1, 1000] 范围内
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root) {
+  let ans = 0;
+
+  const traverse = (r, p) => {
+    if (r == null) return;
+
+    if (r.left == null && r.right == null && p == 'left') {
+      ans += r.val;
+    }
+
+    traverse(r.left, 'left');
+    traverse(r.right, 'right');
+  };
+
+  traverse(root);
+
+  return ans;
+};
+// @lc code=end
