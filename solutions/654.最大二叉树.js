c8e1f9c7243ced7993dd7f09bb64403c8360709c
kaichi <hellokitty321ds@gmail.com>
2022-09-20 13:45:20 +0800

    654

---
+++ solutions/654.最大二叉树.js
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=javascript
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode.cn/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (83.01%)
+ * Likes:    567
+ * Dislikes: 0
+ * Total Accepted:    154.9K
+ * Total Submissions: 186.7K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+ *
+ *
+ * 创建一个根节点，其值为 nums 中的最大值。
+ * 递归地在最大值 左边 的 子数组前缀上 构建左子树。
+ * 递归地在最大值 右边 的 子数组后缀上 构建右子树。
+ *
+ *
+ * 返回 nums 构建的 最大二叉树 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * nums 中的所有整数 互不相同
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function (nums) {
+  function make(l, h) {
+    if (l > h) return null;
+
+    const index = maxIndex(l, h);
+
+    return new TreeNode(nums[index], make(l, index - 1), make(index + 1, h));
+  }
+
+  function maxIndex(l, h) {
+    if (l > h) return -1;
+
+    let ans = -1,
+      max = -Infinity;
+
+    for (let index = l; index <= h; index++) {
+      if (nums[index] > max) {
+        ans = index;
+      }
+      max = Math.max(max, nums[index]);
+    }
+
+    return ans;
+  }
+
+  return make(0, nums.length - 1);
+};
+
+// @lc code=end
