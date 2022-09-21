dd1a1aa276bd5ed0c7ded8b55ef2bed1125c116d
kaichi <hellokitty321ds@gmail.com>
2022-09-21 17:13:17 +0800

    105

---
+++ solutions/105.从前序与中序遍历序列构造二叉树.js
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=javascript
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (71.36%)
+ * Likes:    1733
+ * Dislikes: 0
+ * Total Accepted:    419.7K
+ * Total Submissions: 588K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder
+ * 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * 输出: [3,9,20,null,null,15,7]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: preorder = [-1], inorder = [-1]
+ * 输出: [-1]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder 和 inorder 均 无重复 元素
+ * inorder 均出现在 preorder
+ * preorder 保证 为二叉树的前序遍历序列
+ * inorder 保证 为二叉树的中序遍历序列
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  const inorderMap = new Map();
+
+  inorder.forEach((i, index) => {
+    inorderMap.set(i, index);
+  });
+
+  function build(preorder, pS, pE, inorder, iS, iE) {
+    if (pS > pE) return null;
+
+    const index = inorderMap.get(preorder[pS]);
+
+    const size = index - iS;
+
+    return new TreeNode(
+      preorder[pS],
+      build(preorder, pS + 1, pS + size, inorder, iS, index - 1),
+      build(preorder, pS + size + 1, pE, inorder, index + 1, iE)
+    );
+  }
+
+  return build(
+    preorder,
+    0,
+    preorder.length - 1,
+    inorder,
+    0,
+    inorder.length - 1
+  );
+};
+// @lc code=end
