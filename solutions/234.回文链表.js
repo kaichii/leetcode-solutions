0fd7ffdad62e0857a6c1dfd6454ee40ad6afdd2f
kaichi <hellokitty321ds@gmail.com>
2022-09-29 13:52:26 +0800

    234

---
+++ solutions/234.回文链表.js
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=javascript
+ *
+ * [234] 回文链表
+ *
+ * https://leetcode.cn/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (52.50%)
+ * Likes:    1528
+ * Dislikes: 0
+ * Total Accepted:    509.2K
+ * Total Submissions: 969.8K
+ * Testcase Example:  '[1,2,2,1]'
+ *
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,2,1]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2]
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点数目在范围[1, 10^5] 内
+ * 0 <= Node.val <= 9
+ *
+ *
+ *
+ *
+ * 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  let slow = head,
+    fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  if (fast !== null) {
+    slow = slow.next;
+  }
+
+  let left = head;
+  let right = reverse(slow);
+
+  while (right !== null) {
+    if (right.val !== left.val) return false;
+
+    left = left.next;
+    right = right.next;
+  }
+
+  return true;
+};
+
+var reverse = function (head) {
+  let pre = null;
+  let curr = head;
+
+  while (curr !== null) {
+    const next = curr.next;
+
+    curr.next = pre;
+    pre = curr;
+    curr = next;
+  }
+
+  return pre;
+};
+// @lc code=end
