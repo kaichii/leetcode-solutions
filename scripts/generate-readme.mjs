e5a92ec55a9a794c54d07726e3a5b4cb57269710
kaichi <hellokitty321ds@gmail.com>
2022-09-29 11:18:47 +0800

    Generate Doc

---
+++ scripts/generate-readme.mjs
@@ -0,0 +1,50 @@
+import markdownMagic from 'markdown-magic';
+import path from 'path';
+import fs from 'fs';
+import { markdownTable } from 'markdown-table';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const difficulty = {
+  Hard: '🔴',
+  Medium: '🟠',
+  Easy: '🟢',
+};
+const markdownPath = path.resolve(__dirname, '../README.md');
+const solutions = path.resolve(__dirname, '../solutions');
+
+const config = {
+  transforms: {
+    /* Match <!-- AUTO-GENERATED-CONTENT:START (SOLUTIONS) --> */
+    SOLUTIONS() {
+      const rows = [['Order', 'Solution', 'Difficulty', 'Source']];
+      fs.readdirSync(solutions)
+        .sort((a, b) => a.split('.')[0] - b.split('.')[0])
+        .forEach(async (fileName) => {
+          const order = fileName.split('.')[0];
+          const filePath = path.join(solutions, fileName);
+          const content = fs.readFileSync(filePath, 'utf8');
+
+          const title = content
+            .match(/(\[\d{1,}\].+)/)[0]
+            .replace(/\[\d{1,}\] /, '');
+          const d = difficulty[content.match(/Hard|Medium|Easy/)[0]];
+          const url = content.match(/https:\/\/.+problems.+/)[0];
+
+          rows.push([
+            order,
+            `[${title}](./solutions/${fileName})`,
+            d,
+            `[题目详情](${url})`,
+          ]);
+        });
+
+      return markdownTable(rows, { align: ['c', 'l', 'c', 'l'] });
+    },
+  },
+};
+
+markdownMagic(markdownPath, config);
