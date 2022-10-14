984ed17966ae03bb9202237df9fd2b4a6ef005ec
kaichi <hellokitty321ds@gmail.com>
2022-10-14 15:02:34 +0800

    25

---
+++ solutions/25.k-个一组翻转链表.js
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=javascript
+ *
+ * [25] K 个一组翻转链表
+ *
+ * https://leetcode.cn/problems/reverse-nodes-in-k-group/description/
+ *
+ * algorithms
+ * Hard (67.68%)
+ * Likes:    1816
+ * Dislikes: 0
+ * Total Accepted:    401.4K
+ * Total Submissions: 593.1K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ *
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中的节点数目为 n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ *
+ *
+ *
+ *
+ * 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+  if (head == null) return head;
+
+  let a = head,
+    b = head;
+
+  for (let i = 0; i < k; i++) {
+    if (b == null) return head;
+    b = b.next;
+  }
+
+  const newHead = reverseBetween(a, b);
+
+  a.next = reverseKGroup(b, k);
+
+  return newHead;
+};
+
+var reverseBetween = function (a, b) {
+  let pre = null,
+    curr = a;
+
+  while (curr !== b) {
+    const next = curr.next;
+
+    curr.next = pre;
+    pre = curr;
+    curr = next;
+  }
+
+  return pre;
+};
+// @lc code=end
