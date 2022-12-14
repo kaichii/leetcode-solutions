3bbabe0332c2c4fb6f948b4fb3bc701451ebd3a3
kaichi <hellokitty321ds@gmail.com>
2022-12-14 14:47:08 +0800

    700

--- solutions/700.二叉搜索树中的搜索.js
+++ solutions/700.二叉搜索树中的搜索.js
@@ -64,8 +64,15 @@
  */
 var searchBST = function (root, val) {
   if (root == null) return null;
-  if (root.val == val) return root;
 
-  return searchBST(root.left, val) || searchBST(root.right, val);
+  if (root.val > val) {
+    return searchBST(root.left, val);
+  }
+
+  if (root.val < val) {
+    return searchBST(root.right, val);
+  }
+
+  return root;
 };
 // @lc code=end
