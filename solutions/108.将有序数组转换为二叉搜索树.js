8299afa097fe69dcf3f5d5de8d6d59152c574925
kaichi <hellokitty321ds@gmail.com>
2022-12-16 11:41:58 +0800

    108

---
+++ solutions/108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=javascript
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ *
+ * https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (77.15%)
+ * Likes:    1152
+ * Dislikes: 0
+ * Total Accepted:    300.7K
+ * Total Submissions: 389.8K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ *
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [-10,-3,0,5,9]
+ * 输出：[0,-3,9,-10,null,5]
+ * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+ *
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,3]
+ * 输出：[3,1]
+ * 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 按 严格递增 顺序排列
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  const buildTree = (nums, lo, hi) => {
+    if (lo > hi) return null;
+
+    const mid = ~~(lo + (hi - lo) / 2);
+
+    const node = new TreeNode(nums[mid]);
+
+    node.left = buildTree(nums, lo, mid - 1);
+    node.right = buildTree(nums, mid + 1, hi);
+
+    return node;
+  };
+
+  return buildTree(nums, 0, nums.length - 1);
+};
+// @lc code=end
