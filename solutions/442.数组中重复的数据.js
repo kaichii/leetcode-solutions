fbdf027f01559e04516d18d4481af2c597ee0760
kaichi <hellokitty321ds@gmail.com>
2023-01-17 11:30:48 +0800

    442

---
+++ solutions/442.数组中重复的数据.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=javascript
+ *
+ * [442] 数组中重复的数据
+ *
+ * https://leetcode.cn/problems/find-all-duplicates-in-an-array/description/
+ *
+ * algorithms
+ * Medium (74.95%)
+ * Likes:    679
+ * Dislikes: 0
+ * Total Accepted:    103.2K
+ * Total Submissions: 137.6K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * 给你一个长度为 n 的整数数组 nums ，其中 nums 的所有整数都在范围 [1, n] 内，且每个整数出现 一次 或 两次 。请你找出所有出现
+ * 两次 的整数，并以数组形式返回。
+ *
+ * 你必须设计并实现一个时间复杂度为 O(n) 且仅使用常量额外空间的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[2,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：[1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1]
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 10^5
+ * 1 <= nums[i] <= n
+ * nums 中的每个元素出现 一次 或 两次
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  const res = [];
+
+  for (const num of nums) {
+    if (nums[Math.abs(num) - 1] < 0) {
+      res.push(Math.abs(num));
+    } else {
+      nums[Math.abs(num) - 1] *= -1;
+    }
+  }
+
+  return res;
+};
+// @lc code=end
